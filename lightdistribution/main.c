2e805c42641df68104a0b5fb8227258f062a33b8
lixiaole <lixiaole2015@gmail.com>
2019-09-26 22:13:22 +0800

    none

--- lightdistribution/main.c
+++ lightdistribution/main.c
@@ -2,17 +2,19 @@
 #include <math.h>
 #include <string.h>
 #include <time.h>
+#include <stdlib.h>
 
 #define length 20 //length of the room
 #define width 10  //width of the room
 #define hight 5   //hight the room
-#define I 10 //identical
-#define N 2   //number of the LED
+#define I 1 //identical
+#define N 1  //number of the LED
 #define DEBUG
 
 int LED[width + 1][length + 1] = {0};   //store LED location
 int LEDtemp[width + 1][length + 1] = {0};   //store LED best location
 double space[width + 1][length + 1] = {0.0}; //store the illumination of every place
+double bestspace[width + 1][length + 1] = {0.0}; //store the best illumination of every place
 double E[width + 1][length + 1][width + 1][length + 1] = {0};   //store the pre-trained data of illumination
 double Umax = 0;  //store the max of U in each epoch
 int w = 0;
@@ -24,7 +26,7 @@ void pretrain() {
         for (j = 0; j < length + 1; j++) {
             for (s = 0; s < width + 1; s++) {
                 for (t = 0; t < length + 1; t++) {
-                    r = sqrt(25 + sqrt(pow(i - s, 2) + pow(j - t, 2)));
+                    r = sqrt(pow(hight, 2) + pow(i - s, 2) + pow(j - t, 2));
                     E[i][j][s][t] = I * hight / pow(r, 3);
                 }
             }
@@ -34,6 +36,13 @@ void pretrain() {
     printf("\npretrain finished!\n");
 }
 
+void randomLED(int n) {
+    srand(time(0));
+    int i, index;
+    for (i = 0; i < n; i++) {
+        index = rand() % ((length + 1) * (width + 1) - 1);
+    }
+}
 
 void calculateAllE() {
     int i, j, s, t;
@@ -92,69 +101,16 @@ void displayLED() {
     }
 }
 
-void displayspace() {
+void displaybestspace() {
     int i, j;
     for (i = 0; i < width + 1; i++) {
         for (j = 0; j < length + 1; j++) {
-            printf("%lf ", space[i][j]);
+            printf("%0.2lf ", bestspace[i][j]);
         }
         printf("\n");
     }
 }
 
-
-void findLED1() {
-    int i, j, x, y;
-    double E = 0.0, u;
-    for (i = 0; i < width + 1; i++) {
-        for (j = 0; j < length + 1; j++) {
-            LED[i][j] = 1;
-            calculateAllE();
-            u = calculateU();
-            if (u > E) {
-                E = u;
-                x = i;
-                y = j;
-            }
-            LED[i][j] = 0;
-        }
-    }
-    printf("LEDs location are : (%d,%d)\n", x, y);
-    LED[x][y] = 1;
-}
-
-void findLED2() {
-    int x1, y1, x2, y2;
-    int i, j, s, t;
-    double E = 0.0, u;
-    for (i = 0; i < width + 1; i++) {
-        for (j = 0; j < length + 1; j++) {
-            for (s = 0; s < width + 1; s++) {
-                for (t = 0; t < length + 1; t++) {
-                    LED[i][j] = 1;
-                    LED[s][t] = 1;
-                    calculateAllE();
-                    u = calculateU();
-#ifdef DEBUG
-                    w++;
-#endif
-                    if (u > E) {
-                        E = u;
-                        x1 = i;
-                        y1 = j;
-                        x2 = s;
-                        y2 = t;
-                    }
-                    LED[i][j] = 0;
-                    LED[s][t] = 0;
-                }
-            }
-        }
-    }
-    printf("LEDs location are : (%d,%d) (%d,%d)\n", x1, y1, x2, y2);
-    LED[x1][y1] = 1;
-    LED[x2][y2] = 1;
-}
 
 void findLED(int n, int s) {
     int i, j;
@@ -166,17 +122,52 @@ void findLED(int n, int s) {
                 if (n > 1) {
                     findLED(n - 1, i * (length + 1) + j + 1);
                 } else if (n == 1) {
+#ifdef DEBUG1
+                    display();
+                    printf("\n\n");
                     w++;
+#endif
                     calculateAllE();
                     u = calculateU();
                     if (u > Umax) {
                         Umax = u;
                         memcpy(LEDtemp, LED, sizeof(LED));
+                        memcpy(bestspace, space, sizeof(space));
                     }
                     LED[i][j] = 0;
                 }
                 LED[i][j] = 0;
             }
+        }
+    }
+}
+
+void findLED1(int n, int k) {
+    int i, j;
+    double u;
+    int istart = (width / n) * k, iend = ((width / n) * (k + 1)) + 1;
+    for (i = istart; i < iend; i++) {
+        for (j = 0; j < length + 1; j++) {
+            if (LED[i][j] == 1) continue;
+            LED[i][j] = 1;
+            if (n > k + 1) {
+                findLED1(n, k + 1);
+            } else if (n == k + 1) {
+#ifdef DEBUG1
+                displayLED();
+                printf("\n");
+                w++;
+#endif
+                calculateAllE();
+                u = calculateU();
+                if (u > Umax) {
+                    Umax = u;
+                    memcpy(LEDtemp, LED, sizeof(LED));
+                }
+                LED[i][j] = 0;
+            }
+            LED[i][j] = 0;
+
         }
     }
 }
@@ -187,8 +178,11 @@ int main() {
     findLED(N, 0);
     display();
     printf("\n\n");
+    displaybestspace();
+    printf("\n\n");
 #ifdef DEBUG
     printf("%d\n", w);
+    printf("%lf\n", Umax);
 #endif
     return 0;
 }
